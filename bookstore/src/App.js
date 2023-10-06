dd2e2699f0e9f5b5a47cff79887d033f5a4b72e7
Mahua2737 <mahua.saikia@mca.christuniversity.in>
2023-10-06 13:41:50 +0530

    dfghjkl

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,10 +1,60 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import SearchForm from './components/SearchForm';
+import BookList from './components/BookList';
+import ReviewForm from './components/ReviewForm';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
+  const fetchBooks = (searchTerm) => {
+
+    const API_KEY = 'AIzaSyD0sYI1saQwsvVnBcZtUAT-o_uP-wcHf-w';
+    const API_URL = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}`;
+
+    axios.get(API_URL)
+      .then((response) => {
+        const bookData = response.data.items.map((item) => {
+          return {
+            id: item.id,
+            title: item.volumeInfo.title,
+            author: item.volumeInfo.authors?.join(', ') || 'Unknown',
+            description: item.volumeInfo.description || 'No description available',
+          };
+        });
+        setBooks(bookData);
+      })
+      .catch((error) => {
+        console.error('Error fetching books:', error);
+        setBooks([]);
+      });
+  };
+
+  const submitReview = (reviewText) => {
+    setReviews([...reviews, reviewText]);
+  };
+
+  useEffect(() => {
+    fetchBooks('React JS'); // Initial search when the app loads
+  }, []);
+
   return (
     <div className="App">
-      sdfghjkl
+      <h1>Online Bookstore</h1>
+      <SearchForm onSearch={fetchBooks} />
+      <BookList books={books} />
+      <h2>Reviews</h2>
+      <ReviewForm onSubmitReview={submitReview} />
+      <div className="reviews">
+        {reviews.map((review, index) => (
+          <div key={index} className="review">
+            <p>{review}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
